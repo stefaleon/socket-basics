da56d03a11ed63d7a6167eaa9799f703b087df5f
maimous@gmail.com <stefaleon@gmail.com>
2016-11-02 18:53:59 +0200

    added front end form to submit messages

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,23 @@ socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
 });
 
-socket.on('message', function(message){
-	console.log('New message!!121!:');
+socket.on('message', function(message) {
+	console.log('New message:');
 	console.log(message.text);
+});
+
+// Handles submitting of new message
+var form = $('#message-form');
+var msg = form.find('input[name=message]');
+
+form.on('submit', function(event) {
+	event.preventDefault();
+	socket.emit('message', {
+		text: msg.val()
+	});
+
+	// clear the form
+	msg.val('');
 
 });
+
